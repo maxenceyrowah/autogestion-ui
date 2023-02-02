d0154b3160bd6c44adc793843161d8047406b7dc
maxence_yrowah <maxenceyrowah30@gmail.com>
2023-02-02 10:26:55 +0000

    initil commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material"
+
+const App = () => {
+  return (
+    <Box className="text-5xl text-center mt-52">
+      Hello world !!
+    </Box>
+  )
+}
+
+export default App
